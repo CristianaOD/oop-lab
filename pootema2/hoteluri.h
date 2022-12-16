11c323b11ddb26c0f914484612caa05c4efa0c4f
CristianaOD <ojocdiana@gmail.com>
2022-12-16 23:00:26 +0200

    Create hoteluri.h

---
+++ pootema2/hoteluri.h
@@ -0,0 +1,22 @@
+#ifndef _HOTELURI_H_
+#define _HOTELURI_H_
+#include "hotel.h"
+
+class Hoteluri {
+
+public:
+    enum HoteluriEnum
+    {
+        ANGAJAT,
+        CLIENT,
+        NUM_ELEMS,
+        NONE
+    };
+    static Hotel* citesteHoteluri(void);
+};
+#endif /* _HOTELURI_H_ */
+
+#ifndef TEMA2POO_ORG_H
+#define TEMA2POO_ORG_H
+
+#endif //TEMA2POO_ORG_H
